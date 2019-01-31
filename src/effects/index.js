0887d4fa791f886a77b26b69f3db7c685e1ffd6f
John Salis <jsalis531@gmail.com>
2019-01-30 23:00:05 -0500

    feat: add `mazeGrowth` effect

--- src/effects/index.js
+++ src/effects/index.js
@@ -1,3 +1,4 @@
 
 export { cellularAutomata } from './cellular-automata';
 export { majorityRules } from './majority-rules';
+export { mazeGrowth } from './maze-growth';
---
+++ src/effects/maze-growth.js
@@ -0,0 +1,53 @@
+
+import Direction from '../direction';
+
+export const mazeGrowth = ({ start, replace, runFactor = 0.5, walkable = [] }) => ({ data, random }) => {
+
+	let nodes = [{ x: start.x, y: start.y }];
+	let walkables = Array.isArray(walkable) ? walkable : [ walkable ];
+	let lastDir = null;
+
+	data.set(start.x, start.y, replace);
+
+	while (nodes.length > 0) {
+
+		let node = nodes[ nodes.length - 1 ];
+		let validDirs = Object.values(Direction.CARDINALS).filter(dir => {
+
+			let x = node.x + (dir.x * 2);
+			let y = node.y + (dir.y * 2);
+
+			if (compareIndex(data, x) !== 0 || compareIndex(data[0], y) !== 0) {
+				return false;
+			}
+
+			let el = data.get(x, y);
+			return el !== replace && (walkables.length === 0 || walkables.includes(el));
+		});
+
+		if (validDirs.length > 0) {
+
+			let dir = (validDirs.includes(lastDir) && random() < runFactor)
+				? lastDir
+				: validDirs[ random() * validDirs.length | 0 ];
+
+			let x = node.x + (dir.x * 2);
+			let y = node.y + (dir.y * 2);
+
+			data.set(x, y, replace);
+			data.set(x - dir.x, y - dir.y, replace);
+
+			nodes.push({ x, y });
+			lastDir = dir;
+
+		} else {
+
+			nodes.pop();
+			lastDir = null;
+		}
+	}
+};
+
+function compareIndex(data, index) {
+	return index < 0 ? -1 : index >= data.length ? 1 : 0;
+}
