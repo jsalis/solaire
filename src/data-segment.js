9dfd9df24fc1b07a54416f28aed64d4c8322128c
John Salis <jsalis531@gmail.com>
2020-07-05 22:52:56 -0400

    refactor: change separator char for mutation data

--- src/data-segment.js
+++ src/data-segment.js
@@ -1,191 +1,185 @@
-
-import { isFunction, wrap } from './utils/common';
-import { randomFrom } from './utils/random';
+import { isFunction, wrap } from "./utils/common";
+import { randomFrom } from "./utils/random";
 
 export const DataSegment = {
+    /**
+     * Creates a new data segment object.
+     *
+     * @param   {Number}   size
+     * @param   {Function} random
+     * @param   {Array}    [regions]
+     * @param   {Object}   [position]
+     * @param   {Object}   [bounds]
+     * @param   {Object}   [mutations]
+     * @returns {Object}
+     */
+    create({ size, random, regions, position, bounds, mutations }) {
+        let data = dataWithSize(size);
+        let muta = mutations || {};
 
-	/**
-	 * Creates a new data segment object.
-	 *
-	 * @param   {Number}   size
-	 * @param   {Function} random
-	 * @param   {Array}    [regions]
-	 * @param   {Object}   [position]
-	 * @param   {Object}   [bounds]
-	 * @param   {Object}   [mutations]
-	 * @returns {Object}
-	 */
-	create({ size, random, regions, position, bounds, mutations }) {
-		let data = dataWithSize(size);
-		let muta = mutations || {};
+        return {
+            get(x, y) {
+                if (x >= 0 && x < size && y >= 0 && y < size) {
+                    return data[x][y];
+                } else if (regions) {
+                    let local = localize(bounds, regions, position, x, y);
 
-		return {
-			get(x, y) {
-				if (x >= 0 && x < size && y >= 0 && y < size) {
-					return data[ x ][ y ];
-				} else if (regions) {
-					let local = localize(bounds, regions, position, x, y);
+                    if (local) {
+                        return local.region.data.get(local.x, local.y);
+                    }
+                }
+            },
 
-					if (local) {
-						return local.region.data.get(local.x, local.y);
-					}
-				}
-			},
+            set(x, y, val) {
+                if (x >= 0 && x < size && y >= 0 && y < size) {
+                    data[x][y] = val;
+                } else if (regions) {
+                    let local = localize(bounds, regions, position, x, y);
 
-			set(x, y, val) {
-				if (x >= 0 && x < size && y >= 0 && y < size) {
-					data[ x ][ y ] = val;
-				} else if (regions) {
-					let local = localize(bounds, regions, position, x, y);
+                    if (local) {
+                        local.region.data.set(local.x, local.y, val);
+                    }
+                }
+            },
 
-					if (local) {
-						local.region.data.set(local.x, local.y, val);
-					}
-				}
-			},
+            mutate(x, y, val) {
+                if (x >= 0 && x < size && y >= 0 && y < size) {
+                    muta[`${x},${y}`] = val;
+                    data[x][y] = val;
+                } else if (regions) {
+                    let local = localize(bounds, regions, position, x, y);
 
-			mutate(x, y, val) {
-				if (x >= 0 && x < size && y >= 0 && y < size) {
-					muta[ `${ x }.${ y }` ] = val;
-					data[ x ][ y ] = val;
-				} else if (regions) {
-					let local = localize(bounds, regions, position, x, y);
+                    if (local) {
+                        local.region.data.mutate(local.x, local.y, val);
+                    }
+                }
+            },
 
-					if (local) {
-						local.region.data.mutate(local.x, local.y, val);
-					}
-				}
-			},
+            each(fn) {
+                for (let x = 0; x < size; x++) {
+                    for (let y = 0; y < size; y++) {
+                        fn(data[x][y], x, y);
+                    }
+                }
+            },
 
-			each(fn) {
-				for (let x = 0; x < size; x++) {
-					for (let y = 0; y < size; y++) {
-						fn(data[ x ][ y ], x, y);
-					}
-				}
-			},
+            map(fn) {
+                return data.map((dim, x) =>
+                    dim.map((el, y) => (isFunction(fn) ? fn(el, x, y) : fn))
+                );
+            },
 
-			map(fn) {
-				return data.map((dim, x) => (
-					dim.map((el, y) => (
-						isFunction(fn) ? fn(el, x, y) : fn
-					))
-				));
-			},
+            fill(val) {
+                for (let x = 0; x < size; x++) {
+                    for (let y = 0; y < size; y++) {
+                        data[x][y] = isFunction(val) ? val(data[x][y], x, y) : val;
+                    }
+                }
+            },
 
-			fill(val) {
-				for (let x = 0; x < size; x++) {
-					for (let y = 0; y < size; y++) {
-						data[ x ][ y ] = isFunction(val)
-							? val(data[ x ][ y ], x, y)
-							: val;
-					}
-				}
-			},
+            randomize(entries) {
+                let fn = randomFrom(entries, random);
 
-			randomize(entries) {
-				let fn = randomFrom(entries, random);
+                for (let x = 0; x < size; x++) {
+                    for (let y = 0; y < size; y++) {
+                        data[x][y] = fn();
+                    }
+                }
+            },
 
-				for (let x = 0; x < size; x++) {
-					for (let y = 0; y < size; y++) {
-						data[ x ][ y ] = fn();
-					}
-				}
-			},
+            match(entries) {
+                let results = [];
 
-			match(entries) {
-				let results = [];
+                for (let x = 0; x < size; x++) {
+                    for (let y = 0; y < size; y++) {
+                        for (let entry of entries) {
+                            if (entry.matcher(x, y, this)) {
+                                results.push({ x, y, key: entry.key });
+                                break;
+                            }
+                        }
+                    }
+                }
 
-				for (let x = 0; x < size; x++) {
-					for (let y = 0; y < size; y++) {
-						for (let entry of entries) {
-							if (entry.matcher(x, y, this)) {
-								results.push({ x, y, key: entry.key });
-								break;
-							}
-						}
-					}
-				}
+                return results;
+            },
 
-				return results;
-			},
+            fromArray(array) {
+                for (let x = 0; x < size; x++) {
+                    for (let y = 0; y < size; y++) {
+                        data[x][y] = array[x][y];
+                    }
+                }
+            },
 
-			fromArray(array) {
-				for (let x = 0; x < size; x++) {
-					for (let y = 0; y < size; y++) {
-						data[ x ][ y ] = array[ x ][ y ];
-					}
-				}
-			},
+            toArray() {
+                return this.map((el) => el);
+            },
 
-			toArray() {
-				return this.map(el => el);
-			},
+            hasElementAt(x, y) {
+                return x >= 0 && x < size && y >= 0 && y < size;
+            },
 
-			hasElementAt(x, y) {
-				return x >= 0 && x < size && y >= 0 && y < size;
-			},
+            size() {
+                return size;
+            },
 
-			size() {
-				return size;
-			},
-
-			mutations() {
-				return muta;
-			}
-		};
-	}
+            mutations() {
+                return muta;
+            },
+        };
+    },
 };
 
 function dataWithSize(size, val) {
-	let data = [];
+    let data = [];
 
-	for (let x = 0; x < size; x++) {
-		data[ x ] = [];
-		for (let y = 0; y < size; y++) {
-			data[ x ][ y ] = val;
-		}
-	}
+    for (let x = 0; x < size; x++) {
+        data[x] = [];
+        for (let y = 0; y < size; y++) {
+            data[x][y] = val;
+        }
+    }
 
-	return data;
+    return data;
 }
 
 function localize(bounds, regions, position, x, y) {
-	let region = regions[ position.x ][ position.y ];
-	let local = { x: position.x, y: position.y };
-	let check = compareIndex(region.data, x);
+    let region = regions[position.x][position.y];
+    let local = { x: position.x, y: position.y };
+    let check = compareIndex(region.data, x);
 
-	while (check !== 0) {
-		local.x += check;
-		x -= check * region.data.size();
-		check = compareIndex(region.data, x);
-	}
+    while (check !== 0) {
+        local.x += check;
+        x -= check * region.data.size();
+        check = compareIndex(region.data, x);
+    }
 
-	if (bounds.x.wrap) {
-		local.x = wrap(local.x, bounds.x.min, bounds.x.max);
-	}
+    if (bounds.x.wrap) {
+        local.x = wrap(local.x, bounds.x.min, bounds.x.max);
+    }
 
-	check = compareIndex(region.data, y);
+    check = compareIndex(region.data, y);
 
-	while (check !== 0) {
-		local.y += check;
-		y -= check * region.data.size();
-		check = compareIndex(region.data, y);
-	}
+    while (check !== 0) {
+        local.y += check;
+        y -= check * region.data.size();
+        check = compareIndex(region.data, y);
+    }
 
-	if (bounds.y.wrap) {
-		local.y = wrap(local.y, bounds.y.min, bounds.y.max);
-	}
+    if (bounds.y.wrap) {
+        local.y = wrap(local.y, bounds.y.min, bounds.y.max);
+    }
 
-	let localRegion = regions[ local.x ] && regions[ local.x ][ local.y ];
+    let localRegion = regions[local.x] && regions[local.x][local.y];
 
-	if (localRegion) {
-		return { region: localRegion, x, y };
-	}
+    if (localRegion) {
+        return { region: localRegion, x, y };
+    }
 
-	return null;
+    return null;
 }
 
 function compareIndex(data, index) {
-	return index < 0 ? -1 : index >= data.size() ? 1 : 0;
+    return index < 0 ? -1 : index >= data.size() ? 1 : 0;
 }
--- src/world.js
+++ src/world.js
@@ -1,312 +1,313 @@
+import merge from "merge";
 
-import merge from 'merge';
-
-import * as vec2 from './utils/vec2';
-import * as effects from './effects';
-import { clamp, wrap, isFunction, isObject, deepEntries } from './utils/common';
-import { randomWithSeed, randomFrom } from './utils/random';
-import { Direction } from './direction';
-import { DataSegment } from './data-segment';
-import { Region } from './region';
-import { RegionGenerator } from './region-generator';
+import * as vec2 from "./utils/vec2";
+import * as effects from "./effects";
+import { clamp, wrap, isFunction, isObject, deepEntries } from "./utils/common";
+import { randomWithSeed, randomFrom } from "./utils/random";
+import { Direction } from "./direction";
+import { DataSegment } from "./data-segment";
+import { Region } from "./region";
+import { RegionGenerator } from "./region-generator";
 
 /**
  * @type {Object} The default world config.
  */
 const DEFAULT_CONFIG = {
-	bounds: {
-		x: { min: -Infinity, max: Infinity, wrap: false },
-		y: { min: -Infinity, max: Infinity, wrap: false }
-	},
-	position: {
-		x: 0,
-		y: 0
-	},
-	seed: null,
-	initialData: {},
-	regions: {},
-	regionSize: 32,
-	chooseRegion: ({ regionTypes }) => regionTypes,
-	generate: () => {}
+    bounds: {
+        x: { min: -Infinity, max: Infinity, wrap: false },
+        y: { min: -Infinity, max: Infinity, wrap: false },
+    },
+    position: {
+        x: 0,
+        y: 0,
+    },
+    seed: null,
+    initialData: {},
+    regions: {},
+    regionSize: 32,
+    chooseRegion: ({ regionTypes }) => regionTypes,
+    generate: () => {},
 };
 
 export const World = {
+    /**
+     * Creates a new world object.
+     *
+     * @param   {Object} config
+     * @returns {Object}
+     */
+    create(config) {
+        config = merge.recursive(true, DEFAULT_CONFIG, config);
+        sanitize(config);
 
-	/**
-	 * Creates a new world object.
-	 *
-	 * @param   {Object} config
-	 * @returns {Object}
-	 */
-	create(config) {
-		config = merge.recursive(true, DEFAULT_CONFIG, config);
-		sanitize(config);
+        let { seed } = randomWithSeed(config.seed);
+        let position = vec2.clone(config.position);
+        let data = createDataObject(config);
 
-		let { seed } = randomWithSeed(config.seed);
-		let position = vec2.clone(config.position);
-		let data = createDataObject(config);
+        return {
+            get data() {
+                return data;
+            },
 
-		return {
-			get data() {
-				return data;
-			},
+            get seed() {
+                return seed;
+            },
 
-			get seed() {
-				return seed;
-			},
+            get position() {
+                return vec2.clone(position);
+            },
 
-			get position() {
-				return vec2.clone(position);
-			},
+            get regionTypes() {
+                return Object.keys(config.regions);
+            },
 
-			get regionTypes() {
-				return Object.keys(config.regions);
-			},
+            get regionSize() {
+                return config.regionSize;
+            },
 
-			get regionSize() {
-				return config.regionSize;
-			},
+            getRegion(...args) {
+                let pos = applyBounds(
+                    args.length === 2 ? { x: args[0], y: args[1] } : args[0],
+                    config.bounds
+                );
+                return pos && data[pos.x]?.[pos.y];
+            },
 
-			getRegion(...args) {
-				let pos = applyBounds(
-					args.length === 2 ? { x: args[0], y: args[1] } : args[0],
-					config.bounds
-				);
-				return pos && data[ pos.x ]?.[ pos.y ];
-			},
+            init(arg) {
+                let opt = parseOptions(arg, position);
 
-			init(arg) {
-				let opt = parseOptions(arg, position);
+                for (let x = opt.x.min; x <= opt.x.max; x++) {
+                    for (let y = opt.y.min; y <= opt.y.max; y++) {
+                        setRegion(data, { x, y }, opt, config);
+                    }
+                }
+            },
 
-				for (let x = opt.x.min; x <= opt.x.max; x++) {
-					for (let y = opt.y.min; y <= opt.y.max; y++) {
-						setRegion(data, { x, y }, opt, config);
-					}
-				}
-			},
+            remove(arg) {
+                let opt = parseOptions(arg, position);
 
-			remove(arg) {
-				let opt = parseOptions(arg, position);
+                for (let x = opt.x.min; x <= opt.x.max; x++) {
+                    for (let y = opt.y.min; y <= opt.y.max; y++) {
+                        data[x][y] = undefined;
+                    }
+                }
+            },
 
-				for (let x = opt.x.min; x <= opt.x.max; x++) {
-					for (let y = opt.y.min; y <= opt.y.max; y++) {
-						data[x][y] = undefined;
-					}
-				}
-			},
+            generate(arg) {
+                let opt = parseOptions(arg, position);
+                let regionTypes = Object.keys(config.regions);
+                let regions = [];
 
-			generate(arg) {
-				let opt = parseOptions(arg, position);
-				let regionTypes = Object.keys(config.regions);
-				let regions = [];
+                for (let x = opt.x.min; x <= opt.x.max; x++) {
+                    for (let y = opt.y.min; y <= opt.y.max; y++) {
+                        let pos = { x, y };
+                        let reg = this.getRegion(pos);
 
-				for (let x = opt.x.min; x <= opt.x.max; x++) {
-					for (let y = opt.y.min; y <= opt.y.max; y++) {
-						let pos = { x, y };
-						let reg = this.getRegion(pos);
+                        if (reg) {
+                            regions.push(reg);
+                            initialize(data, pos, config);
+                        }
+                    }
+                }
 
-						if (reg) {
-							regions.push(reg);
-							initialize(data, pos, config);
-						}
-					}
-				}
+                let regionGenerator = RegionGenerator.create({ regionTypes, regions, seed });
+                config.generate({ regions: regionGenerator, effects });
+                mutate(data, position, config);
+            },
 
-				let regionGenerator = RegionGenerator.create({ regionTypes, regions, seed });
-				config.generate({ regions: regionGenerator, effects });
-				mutate(data, position, config);
-			},
+            move(...args) {
+                let dir = args.length === 2 ? { x: args[0], y: args[1] } : args[0];
 
-			move(...args) {
-				let dir = args.length === 2 ? { x: args[0], y: args[1] } : args[0];
+                if (dir.x === 0 && dir.y === 0) {
+                    return;
+                }
 
-				if (dir.x === 0 && dir.y === 0) {
-					return;
-				}
+                let { bounds } = config;
+                let current = vec2.clone(position);
+                position.x = (bounds.x.wrap ? wrap : clamp)(
+                    position.x + dir.x,
+                    bounds.x.min,
+                    bounds.x.max
+                );
+                position.y = (bounds.y.wrap ? wrap : clamp)(
+                    position.y + dir.y,
+                    bounds.y.min,
+                    bounds.y.max
+                );
 
-				let { bounds } = config;
-				let current = vec2.clone(position);
-				position.x = (bounds.x.wrap ? wrap : clamp)(position.x + dir.x, bounds.x.min, bounds.x.max);
-				position.y = (bounds.y.wrap ? wrap : clamp)(position.y + dir.y, bounds.y.min, bounds.y.max);
+                if (vec2.equals(position, current)) {
+                    throw Error("World position out of bounds");
+                }
+            },
 
-				if (vec2.equals(position, current)) {
-					throw Error('World position out of bounds');
-				}
-			},
+            serialize() {
+                let { bounds, regionSize } = config;
+                let initialData = deepEntries(data).reduce((result, [x, y, region]) => {
+                    result[x] = result[x] || {};
+                    result[x][y] = {
+                        type: region.type,
+                        seed: region.seed,
+                        mutations: region.mutations,
+                    };
+                    return result;
+                }, {});
 
-			serialize() {
-				let { bounds, regionSize } = config;
-				let initialData = deepEntries(data).reduce((result, [x, y, region]) => {
-					result[ x ] = result[ x ] || {};
-					result[ x ][ y ] = {
-						type: region.type,
-						seed: region.seed,
-						mutations: region.mutations
-					};
-					return result;
-				}, {});
-
-				return {
-					seed,
-					position,
-					bounds,
-					regionSize,
-					initialData
-				};
-			}
-		};
-	}
+                return {
+                    seed,
+                    position,
+                    bounds,
+                    regionSize,
+                    initialData,
+                };
+            },
+        };
+    },
 };
 
 function sanitize({ bounds, regions }) {
-	if (bounds.x.min > 0 || bounds.y.min > 0) {
-		throw Error('Minimum bounds must not be greater than zero');
-	}
+    if (bounds.x.min > 0 || bounds.y.min > 0) {
+        throw Error("Minimum bounds must not be greater than zero");
+    }
 
-	if (bounds.x.max < 0 || bounds.y.max < 0) {
-		throw Error('Maximum bounds must not be less than zero');
-	}
+    if (bounds.x.max < 0 || bounds.y.max < 0) {
+        throw Error("Maximum bounds must not be less than zero");
+    }
 
-	if (bounds.x.wrap && (!isFinite(bounds.x.min) || !isFinite(bounds.x.max))) {
-		throw Error('Wrapped bounds must define a minimum and maximum');
-	}
+    if (bounds.x.wrap && (!isFinite(bounds.x.min) || !isFinite(bounds.x.max))) {
+        throw Error("Wrapped bounds must define a minimum and maximum");
+    }
 
-	if (bounds.y.wrap && (!isFinite(bounds.y.min) || !isFinite(bounds.y.max))) {
-		throw Error('Wrapped bounds must define a minimum and maximum');
-	}
+    if (bounds.y.wrap && (!isFinite(bounds.y.min) || !isFinite(bounds.y.max))) {
+        throw Error("Wrapped bounds must define a minimum and maximum");
+    }
 
-	if (Object.keys(regions).length === 0) {
-		throw Error('No regions defined');
-	}
+    if (Object.keys(regions).length === 0) {
+        throw Error("No regions defined");
+    }
 }
 
 function createDataObject({ initialData }) {
-	return deepEntries(initialData).reduce((result, [x, y, state]) => {
-		const region = Region.create({
-			position: { x, y },
-			type: state.type,
-			seed: state.seed,
-			mutations: state.mutations
-		});
+    return deepEntries(initialData).reduce((result, [x, y, state]) => {
+        const region = Region.create({
+            position: { x, y },
+            type: state.type,
+            seed: state.seed,
+            mutations: state.mutations,
+        });
 
-		result[ x ] = result[ x ] || {};
-		result[ x ][ y ] = region;
-		return result;
-	}, {});
+        result[x] = result[x] || {};
+        result[x][y] = region;
+        return result;
+    }, {});
 }
 
 function applyBounds(position, bounds) {
-	let min = { x: bounds.x.min, y: bounds.y.min };
-	let max = { x: bounds.x.max, y: bounds.y.max };
-	let pos = vec2.clone(position);
+    let min = { x: bounds.x.min, y: bounds.y.min };
+    let max = { x: bounds.x.max, y: bounds.y.max };
+    let pos = vec2.clone(position);
 
-	if (bounds.x.wrap) {
-		pos.x = wrap(pos.x, bounds.x.min, bounds.x.max);
-	}
+    if (bounds.x.wrap) {
+        pos.x = wrap(pos.x, bounds.x.min, bounds.x.max);
+    }
 
-	if (bounds.y.wrap) {
-		pos.y = wrap(pos.y, bounds.y.min, bounds.y.max);
-	}
+    if (bounds.y.wrap) {
+        pos.y = wrap(pos.y, bounds.y.min, bounds.y.max);
+    }
 
-	if (vec2.intersects(pos, min, max)) {
-		return pos;
-	}
+    if (vec2.intersects(pos, min, max)) {
+        return pos;
+    }
 
-	return undefined;
+    return undefined;
 }
 
 function parseOptions(opt = {}, position) {
-	let def = {
-		x: { min: position.x - 1, max: position.x + 1 },
-		y: { min: position.y - 1, max: position.y + 1 }
-	};
-	return {
-		...opt,
-		x: {
-			min: isObject(opt.x)
-				? Number(opt.x?.min ?? def.x.min)
-				: Number(opt.x ?? def.x.min),
-			max: isObject(opt.x)
-				? Number(opt.x?.max ?? def.x.max)
-				: Number(opt.x ?? def.x.max),
-		},
-		y: {
-			min: isObject(opt.y)
-				? Number(opt.y?.min ?? def.y.min)
-				: Number(opt.y ?? def.y.min),
-			max: isObject(opt.y)
-				? Number(opt.y?.max ?? def.y.max)
-				: Number(opt.y ?? def.y.max),
-		}
-	};
+    let def = {
+        x: { min: position.x - 1, max: position.x + 1 },
+        y: { min: position.y - 1, max: position.y + 1 },
+    };
+    return {
+        ...opt,
+        x: {
+            min: isObject(opt.x) ? Number(opt.x?.min ?? def.x.min) : Number(opt.x ?? def.x.min),
+            max: isObject(opt.x) ? Number(opt.x?.max ?? def.x.max) : Number(opt.x ?? def.x.max),
+        },
+        y: {
+            min: isObject(opt.y) ? Number(opt.y?.min ?? def.y.min) : Number(opt.y ?? def.y.min),
+            max: isObject(opt.y) ? Number(opt.y?.max ?? def.y.max) : Number(opt.y ?? def.y.max),
+        },
+    };
 }
 
 function setRegion(data, position, options, { bounds, regions, chooseRegion, seed }) {
-	let pos = applyBounds(position, bounds);
+    let pos = applyBounds(position, bounds);
 
-	if (pos) {
-		data[ pos.x ] = data[ pos.x ] || {};
+    if (pos) {
+        data[pos.x] = data[pos.x] || {};
 
-		if (!data[ pos.x ][ pos.y ]) {
-			let regionTypes = Object.keys(regions);
-			let random = randomWithSeed([ seed, pos ]);
-			let type = options.type || chooseRegion({
-				position: pos,
-				regionTypes: regionTypes,
-				random: random
-			});
+        if (!data[pos.x][pos.y]) {
+            let regionTypes = Object.keys(regions);
+            let random = randomWithSeed([seed, pos]);
+            let type =
+                options.type ||
+                chooseRegion({
+                    position: pos,
+                    regionTypes: regionTypes,
+                    random: random,
+                });
 
-			if (Array.isArray(type)) {
-				type = randomFrom(type, random)();
-			}
+            if (Array.isArray(type)) {
+                type = randomFrom(type, random)();
+            }
 
-			if (!regionTypes.includes(type)) {
-				throw Error(`Invalid region type "${ type }" has not been defined`);
-			}
+            if (!regionTypes.includes(type)) {
+                throw Error(`Invalid region type "${type}" has not been defined`);
+            }
 
-			data[ pos.x ][ pos.y ] = Region.create({
-				type: type,
-				seed: options.seed,
-				position: pos
-			});
-		}
-	}
+            data[pos.x][pos.y] = Region.create({
+                type: type,
+                seed: options.seed,
+                position: pos,
+            });
+        }
+    }
 }
 
 function initialize(data, position, { bounds, regions, regionSize, seed }) {
-	let pos = applyBounds(position, bounds);
+    let pos = applyBounds(position, bounds);
 
-	if (pos) {
-		let region = data[ pos.x ][ pos.y ];
+    if (pos) {
+        let region = data[pos.x][pos.y];
 
-		region.data = DataSegment.create({
-			size: regionSize,
-			random: randomWithSeed([ seed, region.seed ]),
-			regions: data,
-			position: pos,
-			bounds: bounds,
-			mutations: region.mutations
-		});
+        region.data = DataSegment.create({
+            size: regionSize,
+            random: randomWithSeed([seed, region.seed]),
+            regions: data,
+            position: pos,
+            bounds: bounds,
+            mutations: region.mutations,
+        });
 
-		if (isFunction(regions[ region.type ].init)) {
-			regions[ region.type ].init({
-				data: region.data,
-				random: randomWithSeed([ seed, region.seed ])
-			});
-		}
-	}
+        if (isFunction(regions[region.type].init)) {
+            regions[region.type].init({
+                data: region.data,
+                random: randomWithSeed([seed, region.seed]),
+            });
+        }
+    }
 }
 
 function mutate(data, position, { bounds }) {
-	Object.values(Direction.NEIGHBORS).forEach(dir => { // TODO use generate area
-		let pos = applyBounds(vec2.add(position, dir), bounds);
-		let region = pos && data[ pos.x ]?.[ pos.y ];
+    Object.values(Direction.NEIGHBORS).forEach((dir) => {
+        // TODO use generate area
+        let pos = applyBounds(vec2.add(position, dir), bounds);
+        let region = pos && data[pos.x]?.[pos.y];
 
-		if (pos && region) {
-			Object.entries(region.mutations).forEach(([ key, val ]) => {
-				let [ x, y ] = key.split('.');
-				region.data.set(x, y, val);
-			});
-		}
-	});
+        if (pos && region) {
+            Object.entries(region.mutations).forEach(([key, val]) => {
+                let [x, y] = key.split(",");
+                region.data.set(x, y, val);
+            });
+        }
+    });
 }
