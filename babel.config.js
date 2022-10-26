affef830078414d15a3fb09c37052005ac6107ac
John Salis <jsalis531@gmail.com>
2022-10-26 19:57:09 -0400

    refactor: format

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,9 @@
+module.exports = (api) => {
+    const envConfig = api.env("test")
+        ? { targets: { node: "current" } }
+        : { modules: false, forceAllTransforms: true };
 
-module.exports = api => {
-
-	const envConfig = api.env('test')
-		? { targets: { node: 'current' } }
-		: { modules: false, forceAllTransforms: true };
-
-	return {
-		presets: [
-			['@babel/preset-env', envConfig]
-		]
-	};
+    return {
+        presets: [["@babel/preset-env", envConfig]],
+    };
 };
